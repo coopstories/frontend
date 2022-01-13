4ca8befb060652dba948b93190b3b37d2825fee2
Andres Martin <monkeyandres@protonmail.com>
2022-01-14 00:14:54 +0100

    Style FullStory page

--- src/app/story/FullStory.tsx
+++ src/app/story/FullStory.tsx
@@ -1,5 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useMemo } from 'react'
 import { useQuery } from 'urql'
+import Button from '../../ui/components/Button'
+import BasicLayout from '../../ui/layouts/BasicLayout'
+import { CREATE_STORY } from '../routes'
 
 const AccessFullStoryQuery = /* GraphQL */ `
   query ($id: Int!, $masterPassword: String!) {
@@ -17,12 +20,29 @@ const AccessFullStoryQuery = /* GraphQL */ `
   }
 `
 
+type FullStoryResponse = {
+  accessFullStory: {
+    id: number
+    title: string
+    creatorName: string
+    createdAt: string
+    storyFragments: {
+      id: number
+      contributorName: string
+      content: string
+    }[]
+  }
+}
+
 const FullStoryPage: React.FC<{ params: { storyId: string } }> = ({
   params,
 }) => {
-  const [masterPassword, setMasterPassword] = useState<string | null>(null)
+  const masterPassword = useMemo(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search)
+    return urlSearchParams.get('masterPassword')
+  }, [])
 
-  const [result] = useQuery({
+  const [result] = useQuery<FullStoryResponse>({
     query: AccessFullStoryQuery,
     variables: { id: parseInt(params.storyId, 10), masterPassword },
     pause: !masterPassword,
@@ -30,44 +50,34 @@ const FullStoryPage: React.FC<{ params: { storyId: string } }> = ({
 
   const { fetching, data, error } = result
 
-  const introducePassword = useCallback(() => {
-    const introducedPassword = prompt('Enter master password:')
-    setMasterPassword(introducedPassword)
-  }, [])
-
-  useEffect(
-    () => introducePassword(),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  )
-
-  if (fetching) {
-    return <p>Loading full story...</p>
-  }
-
-  if (error || !data) {
-    return (
-      <div>
-        <p>Error fetching story</p>
-        <p>{error?.message}</p>
-
-        <button onClick={introducePassword}>Try again</button>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      <h2>
-        {data.accessFullStory.title} by {data.accessFullStory.creatorName}
-      </h2>
+    <BasicLayout
+      customNavBarActions={
+        <Button title="Create new story" to={CREATE_STORY.linkTo()} />
+      }
+    >
+      {fetching ? (
+        <p>Loading full story...</p>
+      ) : error ? (
+        <p>{error.graphQLErrors[0].message}</p>
+      ) : data ? (
+        <>
+          <h1 className="text-3xl mb-6">
+            "{data.accessFullStory.title}" by {data.accessFullStory.creatorName}
+          </h1>
 
-      {data.accessFullStory.storyFragments.map(
-        ({ id, content }: { id: number; content: string }) => (
-          <p key={id}>{content}</p>
-        ),
-      )}
-    </div>
+          <div className="space-y-3">
+            {data.accessFullStory.storyFragments.map(
+              ({ id, content }: { id: number; content: string }) => (
+                <article key={id} className="p-6 rounded-lg bg-light-primary">
+                  <pre className="whitespace-pre-wrap">{content}</pre>
+                </article>
+              ),
+            )}
+          </div>
+        </>
+      ) : null}
+    </BasicLayout>
   )
 }
 

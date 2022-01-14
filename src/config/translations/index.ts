3409c107157c04b4fb493ea2cec470abc5e83363
Andres Martin <monkeyandres@protonmail.com>
2022-01-14 17:27:53 +0100

    Add ES language to Locales enum

--- src/config/translations/index.ts
+++ src/config/translations/index.ts
@@ -1,7 +1,9 @@
 import en from './en.json'
+import es from './es.json'
 
 export enum Locales {
   en = 'en',
+  es = 'es',
 }
 
 type Messages = { [key: string]: string }
@@ -10,4 +12,5 @@ type TranslationsType = { [key in Locales]: Messages }
 
 export const TRANSLATIONS: TranslationsType = {
   [Locales.en]: en,
+  [Locales.es]: es,
 }

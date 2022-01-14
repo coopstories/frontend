92ac012b39d6ca43c91379881feb1cd8b4b38014
Andres Martin <monkeyandres@protonmail.com>
2022-01-14 12:13:45 +0100

    Improve types for useTranslations hook

--- src/hooks/useTranslations.ts
+++ src/hooks/useTranslations.ts
@@ -1,10 +1,12 @@
 import { useCallback } from 'react'
 import { useIntl } from 'react-intl'
 
+export type TranslationFn = (key: string, values?: any) => string
+
 const useTranslations = () => {
   const intl = useIntl()
 
-  const t = useCallback(
+  const t: TranslationFn = useCallback(
     (key: string, values = {}) =>
       intl.formatMessage({ id: key, defaultMessage: key }, values),
     [intl],
